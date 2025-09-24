f1aba21635bc8bf663ddf0d0aedf4903910cca5a
chicoz0 <franciscolemosfisio@gmail.com>
2025-09-24 11:37:09 -0300

    feat: created class Carta

---
+++ exercise_7/Carta.py
@@ -0,0 +1,17 @@
+from AbstractCarta import *
+from Personagem import *
+
+
+class Carta(AbstractCarta):
+
+    def __init__(self, personagem: Personagem):
+        if isinstance(personagem,Personagem):
+            self.__personagem = personagem
+    
+    def valor_total_carta(self) -> int:
+        return(self.__personagem.energia + self.__personagem.habilidade + self.__personagem.resistencia + self.__personagem.velocidade)
+        
+
+    @property
+    def personagem(self) -> Personagem:
+        return self.valor_total_carta()
